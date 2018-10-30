98e243be5ec9aa33ccea501ad38b88ac1a12c206
chandlerpl <c.popelewis@gmail.com>
2018-10-30 21:58:58 +0000

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,21 +3,21 @@ from Gamepad import *
 from Keyboard import *
 ch = 0
 while ch==0:
-    while True:
-        try:
-			self.dev = InputDevice("/dev/input/event"+str(i))
-			if "keyboard" in str(self.dev):
-				break
-        except Exception, e:
-            print "Keyboard not found."
-            break
-            i += 1
-    print "keyboard found "+str(self.dev)
     print 'press 1 to emulate keyboard'
     print 'press 2 to emulate gamepad'
     ch = raw_input("Press any key from menu: ")
     if ch == '1':
         bt = Bluetooth("sdp_record_kbd.xml","000540","BT\ Keyboard")
+		while True:
+			try:
+				self.dev = InputDevice("/dev/input/event"+str(i))
+				if "keyboard" in str(self.dev):
+					break
+			except Exception, e:
+				print "Keyboard not found."
+				break
+				i += 1
+		print "keyboard found "+str(self.dev)
         bt.listen()
         kb = Keyboard()
         kb.event_loop(bt)
