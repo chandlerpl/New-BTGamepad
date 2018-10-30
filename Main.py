5b93f8dd4226a48168dcf77f9fb34ace9939eb1b
chandlerpl <c.popelewis@gmail.com>
2018-10-30 21:53:55 +0000

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,6 +3,16 @@ from Gamepad import *
 from Keyboard import *
 ch = 0
 while ch==0:
+	while True:
+		try:
+	self.dev = InputDevice("/dev/input/event"+str(i))
+	if "keyboard" in str(self.dev):
+				break
+		except Exception, e:
+			print "Keyboard not found."
+			break
+			i += 1
+	print "keyboard found "+str(self.dev)
     print 'press 1 to emulate keyboard'
     print 'press 2 to emulate gamepad'
     ch = raw_input("Press any key from menu: ")
